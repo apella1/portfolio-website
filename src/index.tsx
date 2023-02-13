e9beb006450b65eae9d9751f537d68562de10a3e
apella1 <johncollons5@gmail.com>
2023-02-13 13:57:42 +0300

    Update app routes to latest react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-    );
+  document.getElementById("root") as HTMLElement
+);
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />
+    },
+])
+
 root.render(
-    <React.StrictMode>
-        <Router>
-            <Routes>
-                <Route path='/' element={<App />} />
-                <Route path='home' element={<App />} />
-            </Routes>
-        </Router>
-    </React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider  router={router}/>
+  </React.StrictMode>
 );
