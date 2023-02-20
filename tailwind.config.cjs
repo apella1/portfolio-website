943a34d85cfd96851ca9479a89a66da2a3ccb560
apella1 <johncollons5@gmail.com>
2023-02-20 12:42:55 +0300

    Change background image path

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-	  extend: {
-		  backgroundImage: {
-			  'hero': "url('./static/images/bg.jpg')"
-		  }
-	  },
+    extend: {
+      backgroundImage: {
+        hero: "url('./public/images/bg.jpg')",
+      },
+    },
   },
   plugins: [],
-}
+};
