210fd48717c10898a89c6a62690ec31e12098351
apella1 <johncollons5@gmail.com>
2023-03-25 20:23:30 +0300

    Update footer background and copyright

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,22 +2,29 @@ import { SiLinkedin, SiGithub, SiTwitter, SiWhatsapp } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-center p-4 m-4 space-x-4 md:space-x-6">
-      <a
-        href="https://linkedin.com/in/johnapella"
-        className="text-xl md:text-3xl"
-      >
-        <SiLinkedin />
-      </a>
-      <a href="https://github.com/apella1" className="text-xl md:text-3xl">
-        <SiGithub />
-      </a>
-      <a href="https://twitter.com/apella_john" className="text-xl md:text-3xl">
-        <SiTwitter />
-      </a>
-      <a href="https://wa.me/254796528249" className="text-xl md:text-3xl">
-        <SiWhatsapp />
-      </a>
+    <footer className="flex flex-col text-white bg-[#23272F] p-4 m-4">
+      <div className="flex items-center justify-center space-x-4 md:space-x-6">
+        <a
+          href="https://linkedin.com/in/johnapella"
+          className="text-xl md:text-3xl"
+        >
+          <SiLinkedin />
+        </a>
+        <a href="https://github.com/apella1" className="text-xl md:text-3xl">
+          <SiGithub />
+        </a>
+        <a
+          href="https://twitter.com/apella_john"
+          className="text-xl md:text-3xl"
+        >
+          <SiTwitter />
+        </a>
+        <a href="https://wa.me/254796528249" className="text-xl md:text-3xl">
+          <SiWhatsapp />
+        </a>
+      </div>
+
+      <p className="self-center p-2">&copy; 2023 - John Apella</p>
     </footer>
   );
 };
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,13 +2,10 @@ import { Typewriter } from "react-simple-typewriter";
 import Navbar from "../navbar/Navbar";
 export default function Header() {
   return (
-    <header
-      className="h-screen px-4 py-4 text-white bg-[#23272F]"
-      id="home"
-    >
+    <header className="h-screen p-4  text-white bg-[#23272F]" id="home">
       <Navbar />
       <section className="md:flex p-4">
-        <div className="w-full py-24 pl-6 md:pl-20 space-y-4">
+        <div className="w-full py-24 pl-6 md:pl-20 space-y-2">
           <p className="font-bold text-green-600">Hello, I'm</p>
           <h1 className="text-4xl font-bold md:text-6xl">John Apella</h1>
           <h2 className="text-2xl font-semibold md:text-4xl">
@@ -22,7 +19,7 @@ export default function Header() {
               delaySpeed={1000}
             />
           </h2>
-          
+
           <p className="mt-4">
             <a
               href="#projects"
@@ -32,7 +29,7 @@ export default function Header() {
             </a>
           </p>
         </div>
-        <div className="w-full flex justify-center">
+        <div className="hidden md:w-full md:flex md:justify-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -47,7 +44,6 @@ export default function Header() {
               d="M9.75 3.104v5.714a2.25 2.25 0 01-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 014.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0112 15a9.065 9.065 0 00-6.23-.693L5 14.5m14.8.8l1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0112 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5"
             />
           </svg>
-         
         </div>
       </section>
     </header>
