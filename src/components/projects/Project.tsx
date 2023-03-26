e2ab7942b68c9aebca4724389419c0042d9e54c6
apella1 <johncollons5@gmail.com>
2023-03-26 13:51:59 +0300

    Update projects background

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -11,11 +11,11 @@ export default function Project({ project }: { project: ProjectCard }) {
   const { title, description, stack, site, repo, image } = project;
   return (
     <div className="flex flex-col p-4">
-      <h3 className="self-center inline-block px-4 py-2 text-xl font-bold">
+      <h3 className="self-center text-white inline-block px-4 py-2 text-xl font-bold">
         {title}
       </h3>
-      <img src={image} alt={title.toLowerCase()} className="" />
-      <p className="">{description}</p>
+      <img src={image} alt={title.toLowerCase()} className="text-white" />
+      <p className="text-white">{description}</p>
       <div className="flex self-center mt-auto">
         <ul className="flex flex-wrap list-none list-inside">
           {stack.map((item, id) => (
@@ -23,7 +23,7 @@ export default function Project({ project }: { project: ProjectCard }) {
               key={id}
               className="flex items-center mr-3 opacity-50 text-brand"
             >
-              <span className="small-text">{item.toUpperCase()}</span>
+              <span className="small-text text-white">{item.toUpperCase()}</span>
             </li>
           ))}
         </ul>
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -4,13 +4,13 @@ import { featuredProjects } from "./ProjectsList";
 export default function Projects() {
   return (
     <section
-      className="flex flex-col p-4 m-4 bg-gray-200 rounded-sm space-y-2"
+      className="flex flex-col p-4 m-4 bg-[#1C1F25] rounded-sm space-y-2"
       id="projects"
     >
-      <h2 className="self-center text-4xl font-medium text-green-600">
+      <h2 className="self-center text-4xl font-medium text-white">
         Projects
       </h2>
-      <hr className="bg-green-600 w-40 h-1.5 self-center mb-6 mt-0" />
+      <hr className="bg-white w-40 h-1.5 self-center mb-6 mt-0" />
       <div className="p-4 md:grid md:grid-cols-2 md:gap-6">
         {featuredProjects.map((item, id) => (
           <Project project={item} key={id} />
